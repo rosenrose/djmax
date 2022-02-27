76a6b030ac6e907bae145796402f1603d0f23f70
rosenrose <42435748+rosenrose@users.noreply.github.com>
2022-02-28 01:15:25 +0900

    ㅇ

--- js/random_bga.js
+++ js/random_bga.js
@@ -182,7 +182,7 @@ runBtn.addEventListener("mouseleave", () => {
 resultCount.addEventListener("input", (event) => {
   count = Math.min(parseInt(event.target.value), maxCount);
   if (isNaN(count)) {
-    count = 1;
+    count = 0;
   }
   event.target.value = count;
 });
